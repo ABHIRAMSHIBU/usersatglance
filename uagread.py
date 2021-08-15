457584e300995ac0121b490eb7e61342ab55e080
Abhiram Shibu <abhiramshibu1998@gmail.com>
2021-08-15 19:04:42 +0530

    uagread: Reads log given absolute path to log
    Co-authored-by: Preet Patel <preet.makz@gmail.com>

---
+++ uagread.py
@@ -0,0 +1,24 @@
+import sys
+import pickle
+from pprint import pprint
+if(len(sys.argv)>1):
+    if(sys.argv[1]=="--help" or sys.argv[1]=="-h"):
+        print("Example Usage")
+        print("To open 0th log")
+        print("\t"+sys.argv[0]+" uag.0.log")
+    else:
+        f=open(sys.argv[1],"rb")
+        end = f.seek(0,2)
+        f.seek(0,0)
+        while(True):
+            try:
+                data=pickle.load(f)
+                pprint(data)
+                if(f.tell()==end):
+                    break
+            except:
+                print("Data corruption detected, exiting")
+                break
+            
+else:
+    print(sys.argv[0]+" --help")
